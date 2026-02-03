47f04e7276f83f2a6ce49c5e488fef521b104d10
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 19:37:25 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add a new color'] = '새 색상 추가';
+$lang['Apply the same color to all tags'] = '모든 태그에 동일한 색상 적용';
+$lang['Color'] = '색상';
+$lang['Color added'] = '색상 추가됨';
+$lang['Color deleted'] = '색상 삭제됨';
+$lang['Color saved'] = '색상 저장됨';
+$lang['Display colored tags'] = '색상 태그 표시';
+$lang['Edit color'] = '색상 편집';
+$lang['Everywhere'] = '어디에서나';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = '연관 관계 관리를 위해 <a href="%s">사진/태그</a>로 이동하세요.';
+$lang['Go to <a href="%s">plugin page</a> to edit colors'] = '<a href="%s">플러그인 페이지</a>로 이동하여 색상을 편집하세요';
+$lang['Invalid color'] = '잘못된 색상';
+$lang['Manage colors'] = '색상 관리';
+$lang['New color'] = '새 색상';
+$lang['Only on tags page'] = '태그 페이지만';
+$lang['Set a different color for each tag'] = '각 태그에 다른 색상 설정';
+$lang['Set tags color'] = '태그 색상 설정';
+$lang['This name is already used'] = '이 이름은 이미 사용 중입니다';
+$lang['You must fill all fields (name and color)'] = '모든 필드(이름과 색상)를 입력해야 합니다';
