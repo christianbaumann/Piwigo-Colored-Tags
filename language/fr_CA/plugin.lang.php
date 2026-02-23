aaf0c0ed0b1398044a1ee5f6df309231b25bbb83
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-23 06:53:35 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,13 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Apply the same color to all tags'] = 'Appliquer la même couleur à tous les mots-clés';
+$lang['Display colored tags'] = 'Afficher les mots-clés colorés';
+$lang['Edit color'] = 'Éditer une couleur';
+$lang['Only on tags page'] = 'Uniquement sur la liste des mots-clés';
+$lang['Set a different color for each tag'] = 'Appliquer une couleur différente à chaque mot-clé';
+$lang['Set tags color'] = 'Changer la couleur des mots-clés';
